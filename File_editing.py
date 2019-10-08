7a629e853e92365da9a71035a0dec355fb15a895
sandu <sanduni.madu1996@gmail.com>
2019-10-08 13:42:56 +0530

    read, edit and rewrite file using python

---
+++ File_editing.py
@@ -0,0 +1,11 @@
+with open("yukthi.txt","r+") as f:
+    lines =f.readlines()
+    #pointer to top
+    f.seek(0)
+    #clear file content
+    f.truncate()
+    for line in lines:
+        if not line.startswith('hi'):
+            f.write(line)
+
+f.close()
