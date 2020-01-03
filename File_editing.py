2c9abccc1a37cb2d6d0c79aac433d4a9b33acd02
Sanduni Abenayaka <43136228+SanduniAbenayaka@users.noreply.github.com>
2020-01-03 14:40:52 +0530

    updated

--- File_editing.py
+++ File_editing.py
@@ -1,9 +1,12 @@
 with open("filename.txt","r+") as f:
     lines =f.readlines()
+    
     #pointer to top
     f.seek(0)
+    
     #clear file content
     f.truncate()
+    
     for line in lines:
         if not line.startswith('hi'):
             f.write(line)
